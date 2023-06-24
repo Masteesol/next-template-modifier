87f0d2eabfebde538257622a223e292d50724e59
MariusSolheim <marius@apidari.com>
2023-06-24 14:31:02 +0200

    log check

--- utils/checkEnv.ts
+++ utils/checkEnv.ts
@@ -1,6 +1,8 @@
 const checkEnv = () => {
     const devUrl = process.env.NEXT_PUBLIC_DEV_BASE_URL as string;
     const prodUrl = process.env.NEXT_PUBLIC_BASE_URL as string;
+    console.log("devUrl", devUrl)
+    console.log("prodUrl", prodUrl)
     return process.env.NEXT_PUBLIC_API_ENV === 'development' ? devUrl : prodUrl
 }
 

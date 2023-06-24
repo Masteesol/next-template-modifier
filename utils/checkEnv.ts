ef75be3e3d1b55aa59af2ce20163435b0b202d9f
MariusSolheim <marius@apidari.com>
2023-06-24 15:02:23 +0200

    checkEnv update

--- utils/checkEnv.ts
+++ utils/checkEnv.ts
@@ -3,7 +3,8 @@ const checkEnv = () => {
     const prodUrl = process.env.NEXT_PUBLIC_BASE_URL as string;
     console.log("devUrl", devUrl)
     console.log("prodUrl", prodUrl)
-    return process.env.NEXT_PUBLIC_API_ENV === 'development' ? devUrl : prodUrl
+    const url = process.env.NEXT_PUBLIC_API_ENV === 'development' ? devUrl : prodUrl
+    return url ? url : ""
 }
 
 export default checkEnv

cd00dd40d326bd6b9bc52c4b85a269eb7613d212
MariusSolheim <marius@apidari.com>
2023-07-05 19:58:11 +0200

    FIX: email conf page

--- pages/confirm-email/index.tsx
+++ pages/confirm-email/index.tsx
@@ -1,48 +1,15 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
 import PageLayout from '@/components/landing/PageLayout';
-import { FlexColCentered, H1, H2 } from "@/components/shared/styled-global-components";
-import { Badge } from 'flowbite-react';
+import { FlexColCentered, H1 } from "@/components/shared/styled-global-components";
 import { CheckIcon } from '@/components/shared/CustomIcons';
 
 const ConfirmEmail = () => {
-
-    const router = useRouter();
-    const [error, setError] = useState(false)
-    const [confirmed, setConfirmed] = useState(false)
-    const [redirecting, setRedirecting] = useState(false)
-
-    useEffect(() => {
-        const { access_token } = router.query;
-        console.log(access_token)
-        if (access_token && access_token !== "unauthorized_client") {
-            setConfirmed(true)
-            setTimeout(() => {
-                setRedirecting(true)
-                setTimeout(() => {
-                    router.push("/")
-                }, 1000)
-            }, 2000)
-        } else {
-            setError(true)
-        }
-    }, [router]);
-
     return (
         <PageLayout>
             <FlexColCentered className="h-full gap-8">
-                {error ?
-                    <Badge color={"failure"}>Error validating email </Badge>
-                    :
-                    <H1>Thank you for confirming your email!</H1>
-                }
-                {
-                    confirmed &&
-                    <FlexColCentered className="gap-4">
-                        <CheckIcon />
-                        {redirecting && <H2>Redirecting...</H2>}
-                    </FlexColCentered>
-                }
+                <H1>Thank you for confirming your email!</H1>
+                <FlexColCentered className="gap-4">
+                    <CheckIcon />
+                </FlexColCentered>
             </FlexColCentered>
         </PageLayout>
     );

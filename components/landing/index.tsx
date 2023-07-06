e0d3955784b4af264180bc2612177e6a3c74c2fd
MariusSolheim <marius@apidari.com>
2023-07-06 09:28:26 +0200

    fixed grid on landing page

--- components/landing/index.tsx
+++ components/landing/index.tsx
@@ -59,51 +59,23 @@ const LandingPageContent = () => {
                 </FlexRowCentered>
                 <FlexColCenteredX className="p-4 mt-12">
                     <FlexColContainer className="w-full max-w-[1240px] gap-12">
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                            <FlexColCenteredY className="flex-1 gap-4">
-                                <H2 className="text-green-500">{text.gridContainers[0].title}</H2>
-                                <p className="text-gray-700">{text.gridContainers[0].content}</p>
-                            </FlexColCenteredY>
-                            <div className="shadow rounded flex-1">
-                                <video
-                                    controls
-                                    preload="none"
-                                    width="100%"
-                                    poster="https://res.cloudinary.com/dedym3sfv/image/upload/v1687692796/poster-templify-video_p3rgrm.jpg"
-                                    src="https://res.cloudinary.com/dedym3sfv/video/upload/v1687693768/using_template_modifier_app_jnznvr.mp4"
-                                >
-                                    Your browser does not support the video tag.
-                                </video>
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                            <FlexColCentered>
-                                <Image src={text.gridContainers[2].media.link} alt={`${text.gridContainers[2].media.alt}`} width={400} height={400} />
-                            </FlexColCentered>
-                            <FlexColCenteredY className="gap-4">
-                                <H2 className="text-green-500">{text.gridContainers[2].title}</H2>
-                                <p>{text.gridContainers[2].content}</p>
-                            </FlexColCenteredY>
-                        </div>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                        {text.gridContainers.map((item, index) => {
+                            const isOdd = index % 2 !== 0
+                            const reverseFirst = isOdd ? "md:order-last" : "shadow"
+                            const reverseSecond = isOdd ? "md:order-first" : "shadow"
 
-                            <FlexColCenteredY className="gap-4">
-                                <H2 className="text-green-500">{text.gridContainers[1].title}</H2>
-                                <p>{text.gridContainers[1].content}</p>
-                            </FlexColCenteredY>
-                            <FlexColCentered className="shadow">
-                                <Image src={text.gridContainers[1].media.link} alt={`${text.gridContainers[1].media.alt}`} width={600} height={600} />
-                            </FlexColCentered>
-                        </div>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                            <FlexColCentered>
-                                <Image src={text.gridContainers[3].media.link} alt={`${text.gridContainers[3].media.alt}`} width={400} height={400} />
-                            </FlexColCentered>
-                            <FlexColCenteredY className="gap-4">
-                                <H2 className="text-green-500">{text.gridContainers[3].title}</H2>
-                                <p>{text.gridContainers[3].content}</p>
-                            </FlexColCenteredY>
-                        </div>
+                            return (
+                                <div key={`${index}-grid-item`} className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                                    <FlexColCenteredY className={"flex-1 gap-4" + reverseFirst}>
+                                        <H2 className={"text-green-500"}>{item.title}</H2>
+                                        <p className={"text-gray-700"}>{item.content}</p>
+                                    </FlexColCenteredY>
+                                    <FlexColCentered className={`${reverseSecond}`}>
+                                        <Image src={item.media.link} alt={`${item.media.alt}`} width={isOdd ? 400 : 600} height={isOdd ? 300 : 600} />
+                                    </FlexColCentered>
+                                </div>
+                            )
+                        })}
                     </FlexColContainer>
                 </FlexColCenteredX>
             </FlexColContainer>
@@ -116,3 +88,15 @@ const LandingPageContent = () => {
 }
 
 export default LandingPageContent
+
+/* <div className="shadow rounded flex-1">
+<video
+    controls
+    preload="none"
+    width="100%"
+    poster="https://res.cloudinary.com/dedym3sfv/image/upload/v1687692796/poster-templify-video_p3rgrm.jpg"
+    src="https://res.cloudinary.com/dedym3sfv/video/upload/v1687693768/using_template_modifier_app_jnznvr.mp4"
+>
+    Your browser does not support the video tag.
+</video>
+</div> */
